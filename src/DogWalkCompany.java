3ada13b34b5a306257a156111e754e45e67a3df8
SophieHughes <sophieh39@nycstudents.net>
2025-09-22 13:18:44 -0400

    :)

--- src/DogWalkCompany.java
+++ src/DogWalkCompany.java
@@ -1,2 +1,19 @@
 public class DogWalkCompany {
+    private int[] schedule;
+
+    public DogWalkCompany() {
+        schedule = new int[24];
+    }
+
+    public void changeSchedule(int hour, int dogs) {
+        schedule[hour] += dogs;
+    }
+
+    public int numAvailableDogs(int hour) {
+        return schedule[hour];
+    }
+
+    public void updateDogs(int hour, int numberDogsWalked) {
+        schedule[hour] -= numberDogsWalked;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,5 @@
 public class Main {
+    public static void main(String[] args) {
+        DogWalkCompany bestDogWalkers = new DogWalkCompany();
+    }
 }
